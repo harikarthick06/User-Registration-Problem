694e39c9ed36c6b2a39686fe032755ab384e85f8
hari karthick <harikarthickr20@gmail.com>
2026-05-01 14:24:00 +0530

    UC9 validate all email samples

--- src/com/bridgelabz/UserRegistration.java
+++ src/com/bridgelabz/UserRegistration.java
@@ -8,12 +8,12 @@ public class UserRegistration {
     private static final String LAST_NAME_REGEX = "^[A-Z][a-zA-Z]{2,}$";
 
     private static final String EMAIL_REGEX =
-            "^[a-zA-Z0-9]+([._+-][a-zA-Z0-9]+)*@[a-zA-Z0-9]+\\.[a-zA-Z]{2,4}(\\.[a-zA-Z]{2,4})?$";
+            "^[a-zA-Z0-9]+([._+-][a-zA-Z0-9]+)*@[a-zA-Z0-9]+\\.[a-zA-Z]{2,4}(\\.[a-zA-Z]{2})?$";
 
     private static final String MOBILE_REGEX = "^[0-9]{2}\\s[0-9]{10}$";
 
-   private static final String PASSWORD_REGEX =
-        "^(?=.*[A-Z])(?=.*[0-9])(?=[a-zA-Z0-9]*[^a-zA-Z0-9][a-zA-Z0-9]*$).{8,}$";
+    private static final String PASSWORD_REGEX =
+            "^(?=.*[A-Z])(?=.*[0-9])(?=[a-zA-Z0-9]*[^a-zA-Z0-9][a-zA-Z0-9]*$).{8,}$";
 
     public boolean validateFirstName(String firstName) {
         return Pattern.matches(FIRST_NAME_REGEX, firstName);
--- src/com/bridgelabz/UserRegistrationMain.java
+++ src/com/bridgelabz/UserRegistrationMain.java
@@ -41,6 +41,46 @@ public class UserRegistrationMain {
                 ? "Valid Password"
                 : "Invalid Password");
 
+        System.out.println("\nChecking Email Samples:");
+
+        String[] validEmails = {
+                "abc@yahoo.com",
+                "abc-100@yahoo.com",
+                "abc.100@yahoo.com",
+                "abc111@abc.com",
+                "abc-100@abc.net",
+                "abc.100@abc.com.au",
+                "abc@1.com",
+                "abc@gmail.com.com",
+                "abc+100@gmail.com"
+        };
+
+        String[] invalidEmails = {
+                "abc",
+                "abc@.com.my",
+                "abc123@gmail.a",
+                "abc123@.com",
+                "abc123@.com.com",
+                ".abc@abc.com",
+                "abc()*@gmail.com",
+                "abc@%*.com",
+                "abc..2002@gmail.com",
+                "abc.@gmail.com",
+                "abc@abc@gmail.com",
+                "abc@gmail.com.1a",
+                "abc@gmail.com.aa.au"
+        };
+
+        System.out.println("\nValid Email Samples:");
+        for (String sampleEmail : validEmails) {
+            System.out.println(sampleEmail + " : " + userRegistration.validateEmail(sampleEmail));
+        }
+
+        System.out.println("\nInvalid Email Samples:");
+        for (String sampleEmail : invalidEmails) {
+            System.out.println(sampleEmail + " : " + userRegistration.validateEmail(sampleEmail));
+        }
+
         scanner.close();
     }
 }
